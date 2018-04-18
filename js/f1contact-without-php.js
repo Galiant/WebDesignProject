c39ca842ce3e2e9f8f3f78f64a149dc94ed3dec9
EC2 Default User <ec2-user@ip-172-31-27-9.us-east-2.compute.internal>
2018-04-18 15:45:14 +0000

    about contact pages

---
+++ js/f1contact-without-php.js
@@ -0,0 +1,24 @@
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    $('#contact-form').validator();
+
+
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+              var messageAlert = 'alert-';
+              var messageText = 'Your messsage has been sent. We\'ll get back to you soon. Thanks.';
+                    // let's compose Bootstrap alert box HTML
+              var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+			
+            return false;
+        }
+    })
+});
